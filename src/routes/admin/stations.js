00ce1e3cc2529da261244a67c3bd889e1836b2e4
Shaikh Mohd. Asil Aqueel <asilshaikh123@gmail.com>
2025-12-29 23:02:14 +0530

    Fix loyalty points aggregation by station instead of customer

--- src/routes/admin/stations.js
+++ src/routes/admin/stations.js
@@ -93,10 +93,10 @@ LEFT JOIN connector c ON c.charge_point_id = cp.id
 LEFT JOIN charger_types ct ON ct.id = c.charger_type_id
 
 LEFT JOIN (
-  SELECT customer_id, SUM(points) AS eVolts
+  SELECT station_id, SUM(points) AS eVolts
   FROM loyalty_points
-  GROUP BY customer_id
-) lp ON lp.customer_id = cs.created_by
+  GROUP BY station_id
+) lp ON lp.station_id = cs.id
 
 LEFT JOIN attachment a ON a.station_id = cs.id
 
